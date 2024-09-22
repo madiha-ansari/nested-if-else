7c05ee0bfa3f0e6964c5f10fe19411da737ff408
madiha-ansari <157696198+madiha-ansari@users.noreply.github.com>
2024-09-22 12:51:59 +0500

    nested if else

---
+++ app.js
@@ -0,0 +1,41 @@
+
+// // nested if
+// var userSkills = prompt("what languages you know")
+// var userResidence = prompt("where do you live")
+// var userQualification = prompt("what's your degree")
+// var requiredSkills = "javascript"
+// var requiredResidence = "karachi"
+// var requiredQualification = "BSCS"
+// if (userSkills == requiredSkills) {
+//     if (requiredResidence == userResidence) {
+//         if (requiredQualification == userQualification) {
+//             alert("you are hired")
+//         }
+//         else{
+//             alert("BSCS krlo phr ana")
+//         }
+//     }
+//     else {
+//         alert("karachi ajao")
+//     }
+// }
+// else {
+//     alert("javascript sekh kr ao")
+// }
+
+
+// 2
+
+var ages = 21;
+var isPermissions = true;
+if(ages >= 18){
+    console.log("ap adult han ");
+if(isPermissions){
+    console.log("ap ko entry mil gae ha ");
+}else{
+    console.log("ap ko entry nh mil sakti ");
+}
+}
+else{
+    console.log("ap adalt nh ha");
+}
